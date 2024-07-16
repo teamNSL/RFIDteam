9037f7c1f1b68864f2a5443559c8d9e40f92e914
Kataoka <is0610er@ed.ritsumei.ac.jp>
2024-07-16 19:46:10 +0900

    main作りました．

---
+++ RFID.py
@@ -0,0 +1,7 @@
+#読み取り部
+
+class Reading:
+    def Read(self):
+        print('check')
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+#main
+import keyboard
+
+keylist = []
+
+#キーボード入力．Enterが入力されるまで同じリストに格納．
+
+keylist=input()
+print(keylist)
