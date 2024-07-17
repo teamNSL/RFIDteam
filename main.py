183fb3414de60232e230cc2e6c64c8dad0039f3d
Kataoka <is0610er@ed.ritsumei.ac.jp>
2024-07-17 22:42:47 +0900

    片岡とりあえず完成しました．

--- main.py
+++ main.py
@@ -1,62 +1,83 @@
-#main
+# main
+
+
+# ------------リーチを検知------------#
+
+# ------------リーチを検知------------#
 
 
 # 13行24列の二次元配列を初期化
-rows, cols = 13, 24
+rows, cols = 14, 24
 matrix = [[0 for _ in range(cols)] for _ in range(rows)]
 
-#重複なくタグを読み込む．
-Check=[]#確認用リスト
-for num in range(0,rows,1):
-        while True:
-            check=input()
-            flag = True
-            for num2 in range(0,rows,1):
-                if check==matrix[num2]:
-                    flag= False
-            if flag==True:
-                 matrix[num]=check
-                 break
+# 重複なくタグを読み込む．
+Check = []  # 確認用リスト
+for num in range(0, rows, 1):
+    while True:
+        check = input()
+        flag = True
+        for num2 in range(0, rows, 1):
+            if check == matrix[num2]:
+                flag = False
+        if flag == True:
+            matrix[num] = check
+            break
 
+print("リーチ目はすべて読んだよ")
+# リーチ検知
+reach = []
+while True:
+    reach = input()
+    if reach not in matrix:
+        break
+print("最後の一枚引いたよ")
 
-print("")
+# 手牌を書くリストに格納
+hand_man = ''
+hand_pin = ''
+hand_sou = ''
+hand_honors = ''
 
-
-#置き換え
-if "E280F3372000F00007F59EC3" in matrix:
-    matrix[matrix.index("E280F3372000F00007F59EC3")]="9M"
-if "E280F3372000F00007F5885C" in matrix:
-    matrix[matrix.index("E280F3372000F00007F5885C")]="9P"
-if "E280F3372000F00007F5996F" in matrix:
-    matrix[matrix.index("E280F3372000F00007F5996F")]="9S"
-
+# 手牌追加
 if "E280F3372000F00007F563A8" in matrix:
-    matrix[matrix.index("E280F3372000F00007F563A8")]="1M"
-if "E280F3372000F00007F58EB2" in matrix:
-    matrix[matrix.index("E280F3372000F00007F58EB2")]="1P"
-if "E280F3372000F00007F55C05" in matrix:
-    matrix[matrix.index("E280F3372000F00007F55C05")]="1S"
+    hand_man = hand_man + "1"
+if "E280F3372000F00007F59EC3" in matrix:
+    hand_man = hand_man + '9'
 
+if "E280F3372000F00007F58EB2" in matrix:
+    hand_pin = hand_pin + "1"
+if "E280F3372000F00007F5885C" in matrix:
+    hand_pin = hand_pin+"9"
+
+
+if "E280F3372000F00007F55C05" in matrix:
+    hand_sou = hand_sou + "1"
+if "E280F3372000F00007F5996F" in matrix:
+    hand_sou = hand_sou+"9"
 
 if "E280F3372000F00007F58098" in matrix:
-    matrix[matrix.index("E280F3372000F00007F58098")]="East"
+    hand_honors = hand_honors + "1"
 if "E280F3372000F00007F572A3" in matrix:
-    matrix[matrix.index("E280F3372000F00007F572A3")]="South"
+    hand_honors = hand_honors + "2"
 if "E280F3372000F00007F594B3" in matrix:
-    matrix[matrix.index("E280F3372000F00007F594B3")]="West"
+    hand_honors = hand_honors+"3"
 if "E280F3372000F00007F59B40" in matrix:
-    matrix[matrix.index("E280F3372000F00007F59B40")]="North"
+    hand_honors = hand_honors+"4"
 
 
 if "E280F3372000F00007F5544F" in matrix:
-    matrix[matrix.index("E280F3372000F00007F5544F")]="White"
-if "E280F3372000F00007F5A143" in matrix:
-    matrix[matrix.index("E280F3372000F00007F5A143")]="Red"
+    hand_honors = hand_honors+"5"
 if "E280F3372000F00007F5A52D" in matrix:
-    matrix[matrix.index("E280F3372000F00007F5A52D")]="Green"
+    hand_honors = hand_honors+"6"
+if "E280F3372000F00007F5A143" in matrix:
+    hand_honors = hand_honors+"7"
 
+# ------------格納完了------------#
 
-for num in range(0,rows,1):
-    print(matrix[num])
+# ------------リーチ牌が何かわからない------------#
+matrix[13] = reach
 
-print("1")
+print("マンズ："+hand_man)
+print("ピンズ："+hand_pin)
+print("ソーズ："+hand_sou)
+print("honors："+hand_honors)
--- shanten.py
+++ shanten.py
@@ -1,51 +1,51 @@
 
-#計算
+# 計算
 from mahjong.hand_calculating.hand import HandCalculator
-#麻雀牌
+# 麻雀牌
 from mahjong.tile import TilesConverter
-#役, オプションルール
+# 役, オプションルール
 from mahjong.hand_calculating.hand_config import HandConfig, OptionalRules
-#鳴き
+# 鳴き
 from mahjong.meld import Meld
-#風(場&自)
-from mahjong.constants import EAST, SOUTH, WEST, NORTH#シャンテン数
+# 風(場&自)
+from mahjong.constants import EAST, SOUTH, WEST, NORTH  # シャンテン数
 from mahjong.shanten import Shanten
 
-#各インスタンスを生成
+# 各インスタンスを生成
 shanten = Shanten()
 calculator = HandCalculator()
 
 
-def Shantensuu(tile):   
+def Shantensuu(tile):
 
-    #計算
+    # 計算
     res = shanten.calculate_shanten(tile)
     return res
 
 
-#結果出力用
+# 結果出力用
 def print_hand_result(hand_result):
-     #翻数, 符数
-     print(hand_result.han, hand_result.fu)
-     #点数(ツモアガリの場合[左：親失点, 右:子失点], ロンアガリの場合[左:放銃者失点, 右:0])
-     print(hand_result.cost['main'], hand_result.cost['additional'])
-     #役
-     print(hand_result.yaku)
-     #符数の詳細
+    # 翻数, 符数
+    print(hand_result.han, hand_result.fu)
+    # 点数(ツモアガリの場合[左：親失点, 右:子失点], ロンアガリの場合[左:放銃者失点, 右:0])
+    print(hand_result.cost['main'], hand_result.cost['additional'])
+    # 役
+    print(hand_result.yaku)
+    # 符数の詳細
 
-     #国士無双13面待ちでエラー出ます；；
+    # 国士無双13面待ちでエラー出ます；；
     #  for fu_item in hand_result.fu_details:
     #       print(fu_item)
-     print('')
+    print('')
 
 
-#main
+# main
 
-#-----------------手配管理------------------------#
+# -----------------手配管理------------------------#
 
-#-----------------手配管理------------------------#
+# -----------------手配管理------------------------#
 
-hand_man = '19'
+hand_man = '91'
 hand_pin = '19'
 hand_sou = '19'
 hand_honors = '12345677'
@@ -54,47 +54,48 @@ hand_honors = '12345677'
 tumo = ''
 
 
-#手配の情報を整える
-#アガリ形(man=マンズ, pin=ピンズ, sou=ソーズ, honors=1:東, 2:南, 3:西, 4:北, 5:白, 6:發, 7:中)
-#点数計算に使う
-tiles = TilesConverter.string_to_136_array(hand_man, hand_pin, hand_sou, hand_honors)
+# 手配の情報を整える
+# アガリ形(man=マンズ, pin=ピンズ, sou=ソーズ, honors=1:東, 2:南, 3:西, 4:北, 5:白, 6:發, 7:中)
+# 点数計算に使う
+tiles = TilesConverter.string_to_136_array(
+    hand_man, hand_pin, hand_sou, hand_honors)
 
-#手配（聴牌チェックに使う）
-hand_tiles = TilesConverter.string_to_34_array(hand_man, hand_pin, hand_sou, hand_honors)
+# 手配（聴牌チェックに使う）
+hand_tiles = TilesConverter.string_to_34_array(
+    hand_man, hand_pin, hand_sou, hand_honors)
 
-#------------------オプション設定------------------#
+# ------------------オプション設定------------------#
 
-#アガリ牌(上と同じ)
-#ここだけ引数で指定する必要がある
+# アガリ牌(上と同じ)
+# ここだけ引数で指定する必要がある
 win_tile = TilesConverter.string_to_136_array(honors='7')[0]
 
-#鳴き(なし)
+# 鳴き(なし)
 melds = None
 
-#ドラ(なし)
+# ドラ(なし)
 dora_indicators = None
 
-#オプション(ツモを追加,Falseだとロン)
+# オプション(ツモを追加,Falseだとロン)
 config = HandConfig(is_tsumo=True)
 
-#------------------オプション設定------------------#
+# ------------------オプション設定------------------#
 
 
-#------------------上がれるか判定------------------#
+# ------------------上がれるか判定------------------#
 shan = Shantensuu(hand_tiles)
 print(shan)
 
 match shan:
     case -1:
         print("上がりです")
-        
-        #点数計算
-        result = calculator.estimate_hand_value(tiles, win_tile, melds, dora_indicators, config)
+
+        # 点数計算
+        result = calculator.estimate_hand_value(
+            tiles, win_tile, melds, dora_indicators, config)
         print_hand_result(result)
-
-
 
     case 0:
         print("聴牌です")
 
-#-----------------上がれるか判定------------------#
+# -----------------上がれるか判定------------------#
